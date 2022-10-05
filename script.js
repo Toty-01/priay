ea862f22a733bbbbf913127651aba89c72c0c543
Toty-01 <caucheanthony@gmail.com>
2022-10-05 03:28:30 +0200

    nav done

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+const titreSpans = document.querySelectorAll('h1 span');
+const appName = document.querySelectorAll('.app-name');
+const logo = document.querySelectorAll('.logo');
+
+window.addEventListener('load', () => {
+
+  const TL = gsap.timeline({paused: true});
+
+  TL
+  .staggerFrom(titreSpans, 5, {opacity: 0, ease: "power2.out"}, 0.8)
+  .staggerFrom(appName, 1, {opacity: 0, ease: "power2.out"}, 0.5, '-=1')
+  .staggerFrom(logo, 1, {transform: "scale(0)", ease: "power2.out"}, 0.5, '-=2')
+
+  TL.play();
+
+})
+
 function toggleMenu() {
   const navbar = document.querySelector('.navbar');
   const burger = document.querySelector('.navbar_burger');
