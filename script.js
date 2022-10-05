312fc93a079a809e36a62fad0f80949021c3bbd4
Toty-01 <caucheanthony@gmail.com>
2022-10-05 21:16:49 +0200

    event page

--- script.js
+++ script.js
@@ -7,13 +7,25 @@ window.addEventListener('load', () => {
   const TL = gsap.timeline({paused: true});
 
   TL
-  .staggerFrom(titreSpans, 5, {opacity: 0, ease: "power2.out"}, 0.8)
+  .staggerFrom(titreSpans, 4, {opacity: 0, ease: "power2.out"}, 0.8)
   .staggerFrom(appName, 1, {opacity: 0, ease: "power2.out"}, 0.5, '-=1')
   .staggerFrom(logo, 1, {transform: "scale(0)", ease: "power2.out"}, 0.5, '-=2')
 
   TL.play();
 
 })
+
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector(".navbar").style.top = "0";
+  } else {
+    document.querySelector(".navbar").style.top = "-84px";
+  }
+  prevScrollpos = currentScrollPos;
+}
 
 function toggleMenu() {
   const navbar = document.querySelector('.navbar');
