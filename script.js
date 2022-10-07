2d9add9ca6e45f072fea15fc4792415f8e1c6204
--global <caucheanthony@gmail.com>
2022-10-07 19:39:09 +0200

    contact+sponsors

--- script.js
+++ script.js
@@ -37,3 +37,21 @@ function toggleMenu() {
   })
 }
 toggleMenu();
+
+function copy(copyId){
+  let inputElement = document.createElement("input");
+  inputElement.type = "text";
+  let copyText = document.getElementById(copyId).innerHTML;
+  inputElement.value = copyText;
+  document.body.appendChild(inputElement);
+  inputElement.select();
+  document.execCommand('copy');
+  document.body.removeChild(inputElement);
+  
+  document.getElementById("alert").style.display = "block";
+  setTimeout(function(){
+      document.getElementById("alert").style.display = "none";
+  }, 2000);
+}
+
+AOS.init();
